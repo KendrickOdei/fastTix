7afd8f8a9085a2e270c50d9c1ffd4149984db635
KendrickOdei <odekendrick@gmail.com>
2025-04-20 21:54:16 +0000

    second commit

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -147,7 +147,7 @@ export default function Register() {
         
       };
 
-      const res = await fetch(`${import.meta.env.VITE_API_URL}/api/auth/register}`, {
+      const res = await fetch(`${import.meta.env.VITE_API_BASE_URL}/api/auth/register}`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(payload),
