ade4f26323ddce147171a40f7a265a6468aab3f0
KendrickOdei <165942001+KendrickOdei@users.noreply.github.com>
2025-11-15 22:05:56 +0000

    Update sendEmail.ts

--- backend/src/utils/sendEmail.ts
+++ backend/src/utils/sendEmail.ts
@@ -3,9 +3,7 @@ import nodemailer from 'nodemailer';
 
 export const sendOtpEmail = async (email: string, otp: string) => {
     const transporter = nodemailer.createTransport({
-        host: process.env.SMTP_HOST,       // smtp.office365.com
-        port: Number(process.env.SMTP_PORT), // 587
-        secure: false,                     // true for 465, false for 587
+        service: "gmail",                    // true for 465, false for 587
         auth: {
           user: process.env.SMTP_USER,
           pass: process.env.SMTP_PASS,
@@ -20,10 +18,11 @@ export const sendOtpEmail = async (email: string, otp: string) => {
     
   };
   
-
+transporter.verify((err,success)=>{
+    if(err) console.error('SMTP verify error:', err);
+    else console.log("SMTP Server ready:", err)
+})
 
   await transporter.sendMail(mailOptions);
-transporter.verify((err,success)=>{
-    console.log(err || success)
-})
+
 };
